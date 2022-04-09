6f016389e966b9dba3c6e244550a26a09fa631b8
romy <romy@snowyla.com>
2022-04-09 10:15:50 -0700

    Initial Commit

---
+++ SimpleUI/SimpleUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleUIApp.swift
+//  SimpleUI
+//
+//  Created by Romy Ilano on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
